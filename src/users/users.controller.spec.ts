660b16af631877c062522c2aa6575daacc16a332
Bruno Braga <brunobragaw8t@gmail.com>
2024-02-08 21:19:27 +0000

    fix: run lint on UsersController test and fix CqrsModule dependency

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { CqrsModule } from '@nestjs/cqrs';
 
 describe('UsersController', () => {
   let controller: UsersController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [CqrsModule],
       controllers: [UsersController],
     }).compile();
 
