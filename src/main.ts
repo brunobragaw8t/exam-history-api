950541836f69497151276a1b40e99ae85870097e
Bruno Braga <brunobragaw8t@gmail.com>
2024-02-08 21:40:59 +0000

    fix: remove useless comment

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Enable validation pipe
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
